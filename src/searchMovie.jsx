b4757683ac84a6ac5af06f9fec1b288babcea5d1
Chinmay-395 <chinmaydali395@gmail.com>
2021-03-04 19:50:57 +0530

    fetch request to TMDB API

--- src/searchMovie.jsx
+++ src/searchMovie.jsx
@@ -4,6 +4,18 @@ function SearchMovies() {
   const searchMovies = async (e) => {
     e.preventDefault();
     console.log("submitting");
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=2e13f0f8d91d8a119902a225b1e6be9b&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
