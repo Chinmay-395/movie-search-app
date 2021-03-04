9068f32a27c1c6b5350da65b32a24e080de584d9
Chinmay-395 <chinmaydali395@gmail.com>
2021-03-04 20:19:23 +0530

    seperated the card component

--- src/searchMovie.jsx
+++ src/searchMovie.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import CardComponent from "./CardComponent";
 function SearchMovies() {
   //states- input query,movies
   const [query, setQuery] = React.useState("");
@@ -43,27 +43,9 @@ function SearchMovies() {
       <div className="card-list">
         {movies
           .filter((movie) => movie.poster_path)
-          .map((movie) => {
-            return (
-              <div className="card" key={movie.id}>
-                <img
-                  className="card--image"
-                  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                  alt={movie.title + " poster"}
-                />
-                <div className="card--content">
-                  <h3 className="card--title">{movie.title}</h3>
-                  <p>
-                    <small>RELEASE DATE: {movie.release_date}</small>
-                  </p>
-                  <p>
-                    <small>RATING: {movie.vote_average}</small>
-                  </p>
-                  <p className="card--desc">{movie.overview}</p>
-                </div>
-              </div>
-            );
-          })}
+          .map((movie) => (
+            <CardComponent key={movie.id} movie={movie} />
+          ))}
       </div>
     </>
   );
